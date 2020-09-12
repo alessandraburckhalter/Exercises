5043fc6dd6084d343626ce9c4cc44ae365e6023d
Alessandra Burckhalter <ale.lptc@gmail.com>
2020-09-12 16:45:10 -0400

    new folder

---
+++ algorithms/ex-01-palindrome.js
@@ -0,0 +1,21 @@
+function palindrome(word) {
+    let reverseWord = word.split().reverse().join()
+    if (reverseWord != word) {
+        console.log("It's not a palindrome.")
+    } else  {
+        console.log("It's a palindrome.")
+    }
+}
+
+palindrome("mom")
+
+
+for (let x = 0; x < array.length; x++) {
+    const elementX = array[x];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (element > element)
+            temp = element
+        
+    }
+}
